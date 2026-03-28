36fd7f59475b4bdca9780866c39b51d62d223527
Willian Oliveira <willianb.o.1993@gmail.com>
2026-03-27 21:57:11 -0300

    feat: notificacao selecao cliente - badge pulsante no painel e estrela nas fotos da galeria

--- api/galerias/list.php
+++ api/galerias/list.php
@@ -5,6 +5,7 @@ $u = require_fotografo();
 $sql = "
     SELECT g.*,
            COUNT(i.id) as total_fotos,
+           SUM(CASE WHEN i.selecionada = 1 THEN 1 ELSE 0 END) as total_selecionadas,
            (SELECT i2.caminho_arquivo FROM imagens i2
             WHERE i2.galeria_id = g.id ORDER BY i2.ordem LIMIT 1) as thumb
     FROM galerias g
